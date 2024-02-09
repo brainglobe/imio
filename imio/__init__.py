48a409b895e7a13d3bb3db24d20c78197802a236
willGraham01 <1willgraham@gmail.com>
2024-02-09 10:07:21 +0000

    Add import warning and direct to brainglobe-utils

--- imio/__init__.py
+++ imio/__init__.py
@@ -1,3 +1,12 @@
+from warnings import warn
+
+warn(
+    "imio has merged into brainglobe-utils as a submodule."
+    "To remain up to date, please install brainglobe-utils and use the image_io submodule:"
+    "https://github.com/brainglobe/brainglobe-utils",
+    DeprecationWarning,
+)
+
 __author__ = "Charly Rousseau, Adam Tyson"
 __version__ = "0.2.4"
 
